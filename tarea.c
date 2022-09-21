7cf795a8e0e100953bb5f552fbd77f228d0bf558
unknown <vperezc18@gmail.com>
2022-09-21 19:30:15 -0300

    prueba2

--- tarea.c
+++ tarea.c
@@ -1,7 +1,8 @@
-#include <math.h>
+#include <fcntl.h>      /* Modos de apertura */
+#include <stdlib.h>     /* Funciones de ficheros */
 #include <stdio.h>
 
-int main()
+int main ( int argc, char* argv[] )
 {
     int n, opcion;
 
@@ -21,8 +22,31 @@ int main()
         switch ( opcion )
         {
             case 1:
-                    
-                    break;
+                char cadena[11];	/* Depósito de los caracteres */
+                int leidos;
+
+                /* Apertura del fichero */
+
+                int fichero = open ("/mnt/c/Users/vicho/Desktop/Tarea-sistemas-operativos/Rombo", O_CREAT|O_RONL,0644);
+
+                /* Comprobación */   
+                if (fichero==-1)
+                {
+                        perror("Error al abrir fichero:");
+                        exit(1);
+                }
+
+                /* Coloca el puntero en la posición 400 */
+                lseek(fichero,0,SEEK_SET);
+
+                /* Lee diez bytes */
+                leidos = fread(fichero, cadena, 11);
+                pclose(fichero);
+                cadena[11]=0;
+
+                /* Mensaje para ver qué se leyó */
+                printf ( "Se leyeron %d bytes. La cadena leida es %s\n",leidos,cadena );
+                break;
 
             case 2:
                     
