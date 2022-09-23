43db566831afbcfc51fc2b4750e085ed394175d6
unknown <vperezc18@gmail.com>
2022-09-22 21:17:27 -0300

    orden carpetas

--- tarea.c
+++ tarea.c
@@ -1,66 +1,99 @@
-#include <fcntl.h>      /* Modos de apertura */
-#include <stdlib.h>     /* Funciones de ficheros */
+#include <string.h>
+#include <stdlib.h>
+#include <dirent.h>
 #include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <regex.h>
 
-int main ( int argc, char* argv[] )
+int main(int argc, char *argv[])
 {
-    int n, opcion;
+        DIR *d;
+        struct dirent *dir;
+        d = opendir(".");
+        char *regexstring = "[a-zA-Z]*\\.txt";
+        regex_t regexcompile;
+        regcomp(&regexcompile, regexstring, REG_EXTENDED);
 
-    do
-    {
-        printf( "\n   1. Ver cartas por pinta");
-        printf( "\n   2. Ver cartas por criatura");
-        printf( "\n   3. Editar Pinta");
-        printf( "\n   4. Editar criatura");
-        printf( "\n   5. Salir." );
-        printf( "\n\n   Introduzca opcion (1-5): ");
-
-        scanf( "%d", &opcion );
-
-        /* Inicio del anidamiento */
-
-        switch ( opcion )
+        if (d)
         {
-            case 1:
-                char cadena[11];	/* Depósito de los caracteres */
-                int leidos;
-
-                /* Apertura del fichero */
-
-                int fichero = open ("/mnt/c/Users/vicho/Desktop/Tarea-sistemas-operativos/Rombo", O_CREAT|O_RONL,0644);
-
-                /* Comprobación */   
-                if (fichero==-1)
+                while ((dir = readdir(d)) != NULL)
                 {
-                        perror("Error al abrir fichero:");
-                        exit(1);
+                        int value = regexec(&regexcompile, dir->d_name, 0, NULL, 0);
+                        if (value == 0)
+                        {
+                                char cadena[50]; /* Depósito de los caracteres */
+                                int leidos;
+                                int fichero = open(dir->d_name, O_RDONLY);
+                                if (fichero == -1)
+                                {
+                                        perror("Error al abrir fichero:");
+                                        exit(1);
+                                }
+                                lseek(fichero, 0, SEEK_SET);
+                                char delimitador[] = "\n";
+                                int cont = 1;
+                                /* Lee diez bytes */
+                                leidos = read(fichero, cadena, 50);
+                                close(fichero);
+                                char *token = strtok(cadena, delimitador);
+                                if (token != NULL)
+                                {
+                                        while (token != NULL)
+                                        {
+                                                token = strtok(NULL, delimitador);
+                                                if (cont == 2)
+                                                {
+                                                        mkdir(token, S_IRWXU);
+                                                        char slash[3] = "./";
+                                                        strcat(slash, token);
+                                                        strcat(slash, "/");
+                                                        strcat(slash, dir->d_name);
+                                                        rename(dir->d_name, slash);
+                                                }
+                                                cont++;
+                                        }
+                                }
+                        }
+                }
+                closedir(d);
+        }
+
+        int n, opcion;
+
+        do
+        {
+                printf("\n   1. Ver cartas por pinta");
+                printf("\n   2. Ver cartas por criatura");
+                printf("\n   3. Editar Pinta");
+                printf("\n   4. Editar criatura");
+                printf("\n   5. Salir.");
+                printf("\n\n   Introduzca opcion (1-5): ");
+
+                scanf("%d", &opcion);
+
+                /* Inicio del anidamiento */
+
+                switch (opcion)
+                {
+                case 1:
+                       ;
+                        break;
+
+                case 2:
+
+                        break;
+
+                case 3:
+                        break;
+                case 4:
+                        break;
                 }
 
-                /* Coloca el puntero en la posición 400 */
-                lseek(fichero,0,SEEK_SET);
+                /* Fin del anidamiento */
 
-                /* Lee diez bytes */
-                leidos = fread(fichero, cadena, 11);
-                pclose(fichero);
-                cadena[11]=0;
+        } while (opcion != 5);
 
-                /* Mensaje para ver qué se leyó */
-                printf ( "Se leyeron %d bytes. La cadena leida es %s\n",leidos,cadena );
-                break;
-
-            case 2:
-                    
-                    break;
-
-            case 3: 
-                    break;
-            case 4:
-                    break;
-         }
-
-         /* Fin del anidamiento */
-
-    } while ( opcion != 5 );
-
-    return 0;
+        return 0;
 }
